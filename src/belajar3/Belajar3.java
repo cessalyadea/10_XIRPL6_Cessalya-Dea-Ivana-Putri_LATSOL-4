35a32e260c547264849a94b73b4eee1e338414de
LENOVO <LENOVO@LAPTOP-5DMSOMGE>
2021-08-02 18:05:28 +0700

    https://github.com/cessalyadea/10_XIRPL6_Cessalya-Dea-Ivana-Putri_LATSOL-4.git

--- src/belajar3/Belajar3.java
+++ src/belajar3/Belajar3.java
@@ -22,7 +22,7 @@ public class Belajar3 {
         int updatenilai;
         int totalnilai;
         
-        System.out.println("Deret Aritmatika");
+        
         
         for (int i=0; i<5; i++){
              for(int a=0; a<i; a++){
